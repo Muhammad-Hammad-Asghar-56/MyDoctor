afae2b0c0c5401b69344f44a8fc8d039fead76e7
Umar Jamil <99021340+UMAR-78@users.noreply.github.com>
2024-03-24 13:29:55 +0500

    Commit

--- mydoctor/Controller/nurse.js
+++ mydoctor/Controller/nurse.js
@@ -1,7 +1,8 @@
 
 const Joi = require('joi')
 const NurseDBHandler = require("../Database/nurse")
-
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken');
 
 const signUpValidation = Joi.object({
     fName: Joi.string().required(),
@@ -35,7 +36,6 @@ const deleteNurseValidation = Joi.object({
     id: Joi.number().required()
 })
 const NurseController = {
-
     async signUp(req, res, next) {
         const { error } = signUpValidation.validate(req.body)
         if (error) {
@@ -45,6 +45,7 @@ const NurseController = {
         try {
             const { fName, mI, lName, age, gender, phone, address,userName, password } = req.body;
 
+            
             const obj = await NurseDBHandler.addNurse(fName, lName, mI,userName, password, age, gender, phone, address);
             if (obj != null) {
                 res.status(200).json({ success: true, nurse: obj });
